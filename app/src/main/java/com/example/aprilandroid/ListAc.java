76b968814e09f36625aa0d4caada76614231ce45
Deepak <deepakcse31@gmail.com>
2025-04-17 21:43:12 +0530

    Spineer Activty

---
+++ app/src/main/java/com/example/aprilandroid/ListAc.java
@@ -0,0 +1,40 @@
+package com.example.aprilandroid;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class ListAc extends AppCompatActivity {
+    ListView listView;
+    ArrayList arrayList=new ArrayList();
+    ArrayAdapter arrayAdapter;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_list);
+        listView=findViewById(R.id.listview);
+         arrayList.add("c");
+        arrayList.add("c++");
+        arrayList.add("java");
+        arrayList.add("kotlin");
+
+        arrayAdapter=new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/aprilandroid/SplashScreen.java
+++ app/src/main/java/com/example/aprilandroid/SplashScreen.java
@@ -27,7 +27,7 @@ public class SplashScreen extends AppCompatActivity {
                 String loginstatus=sharedPreferences.getString("loginstatus","false");
                 if (loginstatus.equals("true"))
                 {
-                    Intent intent=new Intent(getApplicationContext(),FramentActivity.class);
+                    Intent intent=new Intent(getApplicationContext(),SpinnerActivity.class);
                     startActivity(intent);
                 }else {
 
